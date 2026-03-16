58d6aed4e4efdd3f234e859261d3b00683a55227
VonNeumann101 <2961978672@qq.com>
2026-03-16 20:42:10 +0800

    Finish hw0a-4

--- hw0a/src/NumberTotal.java
+++ hw0a/src/NumberTotal.java
@@ -19,5 +19,10 @@ public class NumberTotal {
 /* Before running the code, type your answer below.
 
 TODO: Write output here
+24 1
+22 2
+19 3
+15 4
+10 5
 
 Then, click the green play button to check your work. */
