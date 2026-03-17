f2561df9aaa2b727b7c6e2ce897bcd7ea17ef645
VonNeumann101 <2961978672@qq.com>
2026-03-17 18:40:24 +0800

    Dessert printDessert method

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,9 +1,14 @@
 public class Dessert {
     int flavor, price;
     static int numDesserts;
+
     public Dessert(int flavor, int price){
         this.flavor = flavor;
         this.price = price;
+        numDesserts++;
     }
-    
+
+    public void printDessert(){
+        System.out.println(flavor + ' ' + price + ' ' + numDesserts);
+    }
 }
