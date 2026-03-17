21d6d33435594f29df479dee326b9850c1bde0f7
VonNeumann101 <2961978672@qq.com>
2026-03-17 18:35:52 +0800

    Dessert base

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,2 +1,9 @@
 public class Dessert {
+    int flavor, price;
+    static int numDesserts;
+    public Dessert(int flavor, int price){
+        this.flavor = flavor;
+        this.price = price;
+    }
+    
 }
