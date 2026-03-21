3787e7d1d317de1ad44721ac126e97f2f22ffc89
VonNeumann101 <2961978672@qq.com>
2026-03-21 16:03:39 +0800

    Phase2

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -17,7 +17,7 @@ public class BombMain {
             b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2("793227803");
         }
     }
 }
