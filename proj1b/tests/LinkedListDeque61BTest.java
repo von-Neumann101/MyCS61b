c74a051fe05a58953516df7e1f07093f101412b1
VonNeumann101 <2961978672@qq.com>
2026-03-25 19:29:21 +0800

    test equals

--- proj1b/tests/LinkedListDeque61BTest.java
+++ proj1b/tests/LinkedListDeque61BTest.java
@@ -24,5 +24,23 @@ public class LinkedListDeque61BTest {
     }
 
     @Test
-    public void equals
+    public void equalsTest(){
+        Deque61B<Integer> lld1 = new LinkedListDeque61B<>();
+        Deque61B<Integer> lld2 = new LinkedListDeque61B<>();
+        lld1.addLast(1);
+        lld1.addLast(2);
+        lld1.addLast(3);
+        lld1.addLast(4);
+
+        lld2.addLast(1);
+        lld2.addLast(2);
+        lld2.addLast(3);
+        lld2.addLast(4);
+
+        assertThat(lld1.equals(lld2)).isTrue();
+
+        lld1.removeFirst();
+
+        assertThat(lld2.equals(lld1)).isFalse();
+    }
 }
