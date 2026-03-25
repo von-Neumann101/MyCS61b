ec60e4d247dcc4057d6bdd6c978086df47b95cae
VonNeumann101 <2961978672@qq.com>
2026-03-25 16:02:40 +0800

    Test addFirst

--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -13,15 +13,24 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-     @Test
-     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-     void noNonTrivialFields() {
-         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-                 .toList();
+    @Test
+    @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+    void noNonTrivialFields() {
+        List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                .toList();
+        assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+    }
 
-         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-     }
+    @Test
+    public void addFirstTest(){
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst(1);
+        lld1.addFirst(2);
+        lld1.addFirst(3);
+
+        assertThat(lld1.toList()).containsExactly(3, 2, 1).inOrder();
+    }
 
     @Test
     public void toListTest(){
@@ -35,7 +44,7 @@ public class ArrayDeque61BTest {
 
         List<Integer> expected = lld1.toList();
         for (int i = 0; i < expected.size(); i++) {
-            assertThat(lld1.getRecursive(i)).isEqualTo(expected.get(i));
+            assertThat(lld1.get(i)).isEqualTo(expected.get(i));
         }
     }
 
