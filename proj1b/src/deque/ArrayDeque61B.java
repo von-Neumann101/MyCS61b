57fabf0dbecd57f7298d1824d0c140c71cd0b4b2
VonNeumann101 <2961978672@qq.com>
2026-03-25 11:46:52 +0800

    ArrayDeque61B-add & size & isempty

--- proj1b/src/deque/ArrayDeque61B.java
+++ proj1b/src/deque/ArrayDeque61B.java
@@ -5,21 +5,36 @@ import java.lang.Math;
 
 public class ArrayDeque61B<T> implements Deque61B<T> {
 
+    private int nextFirst, nextLast;
     private int size;
     private T[] back;
     public ArrayDeque61B(){
-        back = (T[])new Object[16];
+        nextFirst = 3;
+        nextLast = 4;
         size = 0;
+        back = (T[])new Object[8];
     }
 
     @Override
     public void addFirst(T x) {
-
+        back[nextFirst] = x;
+        size += 1;
+        if(nextFirst == 0){
+            nextFirst = back.length - 1;
+            return;
+        }
+        nextFirst -= 1;
     }
 
     @Override
     public void addLast(T x) {
-
+        back[nextLast] = x;
+        size += 1;
+        if(nextLast == back.length - 1){
+            nextLast = 0;
+            return;
+        }
+        nextFirst += 1;
     }
 
     @Override
@@ -29,12 +44,12 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
