5b594fcfc4a4b3d2d95fb6a2eed150248c4b2d04
VonNeumann101 <2961978672@qq.com>
2026-03-26 09:23:48 +0800

    判断Linklist和ArrayList是否相等

--- proj1b/tests/LinkedListDeque61BTest.java
+++ proj1b/tests/LinkedListDeque61BTest.java
@@ -44,6 +44,27 @@ public class LinkedListDeque61BTest {
     }
 
     @Test
+    public void equalsTest2(){
+        Deque61B<Integer> lld1 = new LinkedListDeque61B<>();
+        Deque61B<Integer> lld2 = new ArrayDeque61B<>();
+        lld1.addLast(1);
+        lld1.addLast(2);
+        lld1.addLast(3);
+        lld1.addLast(4);
+
+        lld2.addLast(1);
+        lld2.addLast(2);
+        lld2.addLast(3);
+        lld2.addLast(4);
+
+        assertThat(lld1.equals(lld2)).isTrue();
+
+        lld1.removeFirst();
+
+        assertThat(lld2.equals(lld1)).isFalse();
+    }
+
+    @Test
     public void toStringTest(){
         Deque61B<Integer> lld1 = new LinkedListDeque61B<>();
         lld1.addLast(1);
