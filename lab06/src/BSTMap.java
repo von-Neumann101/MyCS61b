601bb086361ac401f63eb0c88c6d04d93baf2d46
VonNeumann101 <2961978672@qq.com>
2026-03-30 14:21:25 +0800

    BSTMap.java

---
+++ lab06/src/BSTMap.java
@@ -0,0 +1,91 @@
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K ,V> implements Map61B<K ,V>{
+
+    public BSTMap() {
+
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map already contains the specified key, replaces the key's mapping
+     * with the value specified.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Removes every mapping from this map.
+     */
+    @Override
+    public void clear() {
+
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map. Not required for Lab 7.
+     * If you don't implement this, throw an UnsupportedOperationException.
+     */
+    @Override
+    public Set<K> keySet() {
+        return Set.of();
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present,
+     * or null if there is no such mapping.
+     * Not required for Lab 7. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
