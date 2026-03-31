59e6da26b1034784eac069dfd44b858f14c2bf7e
VonNeumann101 <2961978672@qq.com>
2026-03-31 11:45:40 +0800

    迭代器

--- lab06/src/BSTMap.java
+++ lab06/src/BSTMap.java
@@ -216,41 +216,9 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K ,V> {
         return predecessor;
     }
 
-    /**
-     * Returns an iterator over elements of type {@code T}.
-     *
-     * @return an Iterator.
-     */
-
-    private class BSTMapIterator implements Iterator<K> {
-
-        /**
-         * Returns {@code true} if the iteration has more elements.
-         * (In other words, returns {@code true} if {@link #next} would
-         * return an element rather than throwing an exception.)
-         *
-         * @return {@code true} if the iteration has more elements
-         */
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        /**
-         * Returns the next element in the iteration.
-         *
-         * @return the next element in the iteration
-         * @throws NoSuchElementException if the iteration has no more elements
-         */
-        @Override
-        public K next() {
-            return null;
-        }
-    }
-
     @Override
     public Iterator<K> iterator() {
-        return new BSTMapIterator();
+        return keySet().iterator();
     }
 
     private boolean isLeaf(Node node){
