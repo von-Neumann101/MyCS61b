f26c3d0599d3cb7374a3b54c83b733d5f970ef55
VonNeumann101 <2961978672@qq.com>
2026-04-04 10:22:40 +0800

    ok

--- proj2a/src/main/Main.java
+++ proj2a/src/main/Main.java
@@ -13,15 +13,11 @@ public class Main {
     public static void main(String[] args) {
         NgordnetServer hns = new NgordnetServer();
 
-        /* The following code might be useful to you.
-
         NGramMap ngm = new NGramMap(TOP_49887_WORDS_FILE, TOTAL_COUNTS_FILE);
-
-        */
 
         hns.startUp();
         hns.register("history", new DummyHistoryHandler());
-        hns.register("historytext", new DummyHistoryTextHandler());
+        hns.register("historytext", new HistoryTextHandler(ngm));
 
         System.out.println("Finished server startup! Visit http://localhost:4567/ngordnet_2a.html");
     }
