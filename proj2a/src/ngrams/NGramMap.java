fec45a88b89dfd378a5944a5235ed43836c5706a
VonNeumann101 <2961978672@qq.com>
2026-04-04 10:11:01 +0800

    微调final

--- proj2a/src/ngrams/NGramMap.java
+++ proj2a/src/ngrams/NGramMap.java
@@ -22,8 +22,9 @@ public class NGramMap {
     /**
      * Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME.
      */
-    private Map<String, TimeSeries> words;
-    private TimeSeries counts;
+    private final Map<String, TimeSeries> words;
+    private final TimeSeries counts;
+
     public NGramMap(String wordsFilename, String countsFilename) {
         In wordsFile = new In(wordsFilename);
         In countsFile = new In(countsFilename);
