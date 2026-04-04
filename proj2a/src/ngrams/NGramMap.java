2e35655fc3900c65147c1f180be71ad2abc6aff1
VonNeumann101 <2961978672@qq.com>
2026-04-04 10:40:37 +0800

    lazy!

--- proj2a/src/ngrams/NGramMap.java
+++ proj2a/src/ngrams/NGramMap.java
@@ -141,13 +141,7 @@ public class NGramMap {
      * exist in this time frame, ignore it rather than throwing an exception.
      */
     public TimeSeries summedWeightHistory(Collection<String> words) {
-        TimeSeries word = new TimeSeries();
-        for(String i : words) {
-            if(this.words.get(i) == null) continue;
-            TimeSeries newWord = this.words.get(i);
-            word = word.plus(newWord);
-        }
-        return word.dividedBy(counts);
+        return summedWeightHistory(words, MIN_YEAR, MAX_YEAR);
     }
 
 }
