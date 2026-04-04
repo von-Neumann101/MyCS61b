508e0fd495bc2c2b16591fdedf2836725ea27297
VonNeumann101 <2961978672@qq.com>
2026-04-04 10:18:56 +0800

    finish

---
+++ proj2a/src/main/HistoryTextHandler.java
@@ -0,0 +1,27 @@
+package main;
+
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import ngrams.TimeSeries;
+import ngrams.NGramMap;
+
+import java.util.List;
+
+public class HistoryTextHandler extends NgordnetQueryHandler {
+    private NGramMap map;
+    public HistoryTextHandler(NGramMap map) {
+        this.map = map;
+    }
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+        StringBuilder response = new StringBuilder();
+        for(String s : words) {
+            response.append(map.weightHistory(s, startYear, endYear).toString());
+            response.append("\n");
+        }
+        return response.toString();
+    }
+}
