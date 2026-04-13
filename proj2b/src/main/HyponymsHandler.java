a7f0ade52819322bebb7295b21069edd3ae396b7
VonNeumann101 <2961978672@qq.com>
2026-04-13 19:09:52 +0800

    HyponymsHandler导包

--- proj2b/src/main/HyponymsHandler.java
+++ proj2b/src/main/HyponymsHandler.java
@@ -3,6 +3,13 @@ package main;
 import browser.NgordnetQuery;
 import browser.NgordnetQueryHandler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static main.Main.SMALL_HYPONYM_FILE;
+import static main.Main.SMALL_SYNSET_FILE;
+
 public class HyponymsHandler extends NgordnetQueryHandler {
     @Override
     public String handle(NgordnetQuery q) {
