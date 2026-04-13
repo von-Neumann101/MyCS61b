858e1a64ddfc88a77e94c3d845caee1c57ad3b5f
VonNeumann101 <2961978672@qq.com>
2026-04-13 11:26:08 +0800

    读取synset类

--- proj2b/src/main/Synset.java
+++ proj2b/src/main/Synset.java
@@ -1,4 +1,50 @@
 package main;
 
-public class Synset {
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Synset {
+    Map<Integer, Set<String>> idToWord;
+    Map<String, Set<Integer>> wordToId;
+
+    Synset () {
+        idToWord = new HashMap<>();
+        wordToId = new HashMap<>();
+    }
+
+    void build(String file) {
+        In in = new In(file);
+
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] p = line.split(",");
+
+            int id = Integer.parseInt(p[0].trim());
+            String[] words = p[1].trim().split(" ");
+
+            Set<String> wordSet = new HashSet<>();
+            for (String w: words) {
+                if(w.isEmpty()) continue;
+                wordSet.add(w);
+            }
+            idToWord.put(id, wordSet);
+
+            for (String w: wordSet) {
+                wordToId.put(w, new HashSet<>());
+                wordToId.get(w).add(id);
+            }
+        }
+    }
+
+    Map<Integer, Set<String>> getIdToWord() {
+        return idToWord;
+    }
+
+    Map<String, Set<Integer>> getWordToId() {
+        return wordToId;
+    }
 }
