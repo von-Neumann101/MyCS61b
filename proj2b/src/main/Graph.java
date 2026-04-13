7d9e83eb9782f995a7289d48c60643d309538b49
VonNeumann101 <2961978672@qq.com>
2026-04-13 18:55:15 +0800

    protected

--- proj2b/src/main/Graph.java
+++ proj2b/src/main/Graph.java
@@ -10,28 +10,8 @@ public class Graph {
     Graph() {
         graph = new HashMap<>();
     }
-    /**
-     * 用于从hyponyms.txt中构建图
-     *
-     * @param file 文件地址
-     */
-    private void buildGraph(String file, Graph g) {
-        In in = new In(file);
-        
-        while (in.hasNextLine()) {
-            String line = in.readLine();
-            String[] p = line.split(",");
-            
-            int parent = Integer.parseInt(p[0]);
 
-            for (String s : p) {
-                int child = Integer.parseInt(s);
-                g.addEdge(parent, child);
-            }
-        }
-    }
-
-    private void addEdge(int parent, int child) {
+    void addEdge(int parent, int child) {
         // 如果没有parent，加入到graph中
         if (!graph.containsKey(parent)) {
             graph.put(parent, new HashSet<>());
