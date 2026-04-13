c9003c594766ba1511cea3579e5bd8dc5f63bf57
VonNeumann101 <2961978672@qq.com>
2026-04-13 11:25:42 +0800

    Graph类

---
+++ proj2b/src/main/Graph.java
@@ -0,0 +1,48 @@
+package main;
+
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Graph {
+    private final Map<Integer, Set<Integer>> graph; //值就是父节点，键就是其子节点的集合（下位词）
+
+    Graph() {
+        graph = new HashMap<>();
+    }
+    /**
+     * 用于从hyponyms.txt中构建图
+     *
+     * @param file 文件地址
+     */
+    private void buildGraph(String file, Graph g) {
+        In in = new In(file);
+        
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] p = line.split(",");
+            
+            int parent = Integer.parseInt(p[0]);
+
+            for (String s : p) {
+                int child = Integer.parseInt(s);
+                g.addEdge(parent, child);
+            }
+        }
+    }
+
+    private void addEdge(int parent, int child) {
+        // 如果没有parent，加入到graph中
+        if (!graph.containsKey(parent)) {
+            graph.put(parent, new HashSet<>());
+        }
+        //维护child的出（使得child仍然是一个良定义的节点）
+        if (!graph.containsKey(child)) {
+            graph.put(child, new HashSet<>());
+        }
+        graph.get(parent).add(child);
+    }
+}
