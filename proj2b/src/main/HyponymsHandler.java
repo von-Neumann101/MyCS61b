b1a9bff5424294ad9e3a7b6ae9137e69686a727c
VonNeumann101 <2961978672@qq.com>
2026-04-14 15:09:37 +0800

    构造器

--- proj2b/src/main/HyponymsHandler.java
+++ proj2b/src/main/HyponymsHandler.java
@@ -11,6 +11,21 @@ import static main.Main.SMALL_HYPONYM_FILE;
 import static main.Main.SMALL_SYNSET_FILE;
 
 public class HyponymsHandler extends NgordnetQueryHandler {
+
+    Synset s;
+    Graph g;
+    GraphBuilder gb;
+    Map<String, Set<Integer>> wordToId;
+    Map<Integer, Set<String>> IdToWord;
+
+    public HyponymsHandler() {//在创建对象的时候构造图，防止每次查询都构造
+        gb = new GraphBuilder(SMALL_HYPONYM_FILE);
+        g = gb.buildGraph();
+        s = new Synset(SMALL_SYNSET_FILE);
+        wordToId = s.getWordToId();
+        IdToWord = s.getIdToWord();
+    }
+
     @Override
     public String handle(NgordnetQuery q) {
         return "Hello";
