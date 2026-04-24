de3a163baf5ae21eae0014446b066489f5e671de
VonNeumann101 <2961978672@qq.com>
2026-04-24 10:08:50 +0800

    UnionFindSet

---
+++ proj3/src/core/UnionFind.java
@@ -0,0 +1,48 @@
+package core;
+
+public class UnionFind {
+    private int[] parent;
+    private int[] size;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    /**
+     * 寻找x的根节点
+     * @param x 一个节点
+     * @return x的根节点
+     */
+    public int find(int x) {
+        while (x != parent[x]) {
+            parent[x] = find(x); // 顺手处理
+        }
+        return parent[x];
+    }
+
+    public boolean isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    public void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootB == rootA) return;
+
+        if (size[rootA] < size[rootB]) {
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
+        } else {
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
+        }
+    }
+    
+}
