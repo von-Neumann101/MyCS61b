ae02dd2400487d62aeabbe98b8fc80977be04368
VonNeumann101 <2961978672@qq.com>
2026-04-24 10:23:59 +0800

    graph init

--- proj3/src/core/Graph.java
+++ proj3/src/core/Graph.java
@@ -1,4 +1,13 @@
 package core;
 
 public class Graph {
+    List<Edge> edges;
+
+    public Graph() {
+        edges = new ArrayList<>();
+    }
+
+    public void buildGraph(List<Room> rooms) {
+        
+    }
 }
