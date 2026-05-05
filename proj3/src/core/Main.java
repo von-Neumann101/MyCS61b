9a253365b6764d5aac44fe852e6d94bf3d903c86
VonNeumann101 <2961978672@qq.com>
2026-05-05 09:48:58 +0800

    initMenu&drawMainMenu

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -27,4 +27,37 @@ public class Main {
         ter.renderFrame(w.world);
         System.out.println("awa");
     }
+
+    private static void initMenu(TETile[][] world) {
+        int x = world.length;
+        int y = world[0].length;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                world[i][j] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private static void drawMainMenu() {
+        // 让 StdDraw 坐标和窗口像素大小一致
+        StdDraw.setXscale(0, WIDTH);
+        StdDraw.setYscale(0, HEIGHT);
+
+        StdDraw.enableDoubleBuffering();
+
+        StdDraw.clear(StdDraw.BLACK);
+        StdDraw.setPenColor(StdDraw.WHITE);
+
+        double centerX = WIDTH / 2.0;
+
+        StdDraw.setFont(new Font("Monospaced", Font.BOLD, 36));
+        StdDraw.text(centerX, HEIGHT * 0.85, "CS61B: BYOW");
+
+        StdDraw.setFont(new Font("Monospaced", Font.BOLD, 28));
+        StdDraw.text(centerX, HEIGHT * 0.62, "(N) New Game");
+        StdDraw.text(centerX, HEIGHT * 0.49, "(L) Load Game");
+        StdDraw.text(centerX, HEIGHT * 0.36, "(Q) Quit Game");
+
+        StdDraw.show();
+    }
 }
