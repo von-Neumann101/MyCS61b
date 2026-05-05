c567d1595b8e9a4fac32bbe8d874851d0950c9c9
VonNeumann101 <2961978672@qq.com>
2026-05-05 09:48:05 +0800

    menu

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -2,9 +2,29 @@ package core;
 
 public class Main {
     public static void main(String[] args) {
-
-        // build your own world!
-
-
+        TERenderer ter = new TERenderer();
+        ter.initialize(HEIGHT, WIDTH);
+        TETile[][] menu = new TETile[HEIGHT][WIDTH];
+        initMenu(menu);
+        ter.renderFrame(menu);
+        drawMainMenu();
+        while(true) {
+            while (StdDraw.hasNextKeyTyped()) {
+                char c = StdDraw.nextKeyTyped();
+                c = Character.toLowerCase(c);
+                switch (c) {
+                    case 'n':
+                        break;
+                    case 'l':
+                        break;
+                    case 'q':
+                        break;
+                }
+            }
+        }
+        
+        World w = buildWorld(60, 30);
+        ter.renderFrame(w.world);
+        System.out.println("awa");
     }
 }
