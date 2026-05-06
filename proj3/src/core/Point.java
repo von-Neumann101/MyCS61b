ff294a1d06101a4f5e2f3da5314c6fbd633d8fdd
VonNeumann101 <2961978672@qq.com>
2026-05-06 15:59:22 +0800

    无用功能

--- proj3/src/core/Point.java
+++ proj3/src/core/Point.java
@@ -8,16 +8,4 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
-    public String pointToString() {
-        return "(" + x + "," + y + ")";
-    }
-
-    public static Point stringToPoint(String s) {
-        String inside = s.substring(1, s.length() - 1);
-        String[] parts = inside.split(",");
-        int parsedX = Integer.parseInt(parts[0]);
-        int parsedY = Integer.parseInt(parts[1]);
-        return new Point(parsedX, parsedY);
-    }
 }
