65449f7ddb0e2ba7a3d0eef4b6620e81095dadf0
VonNeumann101 <2961978672@qq.com>
2026-05-07 16:56:44 +0800

    import

--- proj3/src/core/Room.java
+++ proj3/src/core/Room.java
@@ -7,8 +7,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
-import static tileengine.Tileset.FLOOR;
-import static tileengine.Tileset.WALL;
+import static tileengine.Tileset.*;
 
 public class Room {
     TETile[][] world;
