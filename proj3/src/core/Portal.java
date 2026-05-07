34e4ffc43a232dd6c3162ae53950ca3be2847285
VonNeumann101 <2961978672@qq.com>
2026-05-07 15:30:15 +0800

    获得rooms的复制

--- proj3/src/core/Portal.java
+++ proj3/src/core/Portal.java
@@ -3,7 +3,9 @@ package core;
 public class Portal {
     Room location;
     Portal link;
-
+    Point position;
+    static List<Room> rooms_ = copyRooms(rooms);
+    
     Portal(Room r, Portal p) {
         location = r;
         link = p;
