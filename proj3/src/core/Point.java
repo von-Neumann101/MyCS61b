c02031a5cbdffe87c0aa93da44d87c38084431b2
VonNeumann101 <2961978672@qq.com>
2026-05-07 16:41:46 +0800

    equals

--- proj3/src/core/Point.java
+++ proj3/src/core/Point.java
@@ -8,4 +8,16 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj instanceof Point p) {
+            if (p.x == this.x && p.y == this.y) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
